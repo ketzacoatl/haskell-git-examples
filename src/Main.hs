d723d7e8cf7ae2f22a375f67d7638fda9539e045
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-05-08 13:24:48 +0000

    git example: list branches in a repo (this repo)

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,28 @@
 module Main where
 
+import Control.Monad.IO.Class
+  ( liftIO
+  )
+
+import Data.Git
+  ( branchList
+  , withRepo
+  , RefName(..)
+  )
+
+import Data.Git.Storage
+  ( openRepo
+  )
+
+import Data.Set
+  ( Set(..)
+  , toList
+  )
+import Filesystem.Path.CurrentOS 
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Example 1: query and print the list of git branches (in this repo)"
+  repo <- openRepo $ decodeString ".git"
+  branches <- Data.Git.branchList repo
+  mapM_ (putStrLn . refNameRaw) $ toList branches
